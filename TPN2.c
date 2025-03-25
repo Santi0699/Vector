1195435daf1275450d16ebdd1cf302530766444e
Santi0699 <santisabsol06@gmail.com>
2025-03-25 10:23:22 -0300

    Ejercicio 1

--- TPN2.c
+++ TPN2.c
@@ -2,8 +2,33 @@
 #include <stdio.h>
 #include "vector.h"
 
+void vector_add_random(vector* v);
+
 int main ()
 {
-    printf("Hola mundo");
+    vector* vectorsito=vector_init(50);
+    vector_add_random(vectorsito);
+    int dim=vector_size(vectorsito);
+    for(int i=0; i<dim;i++)
+    {
+        int value=vector_get(vectorsito,i);
+        printf("%d ",value);
+    }
     return 0;
+}
+
+
+void vector_add_random(vector* v)
+{
+    if (v==NULL)return;
+
+    int i=0;
+    while(!vector_isfull(v))
+    {
+        int value=rand()%100;
+        int temp=vector_set(v,i,value);
+        v->size++;
+        i++;
+    }
+
 }
--- vector.h
+++ vector.h
@@ -3,11 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define T_Elem int
 
 
 typedef struct {
     
-    int* a;
+    T_Elem* a;
     int size;
     int maxsize;
 
@@ -36,6 +37,13 @@ int vector_size(vector*v)
     return v->size;
 }
 
+int vector_max_size(vector*v)
+{
+    if (v==NULL) return -1;
+
+    return v->maxsize;
+}
+
 int vector_isfull(vector* v)
 {
     if (v==NULL) return -1;
@@ -59,7 +67,7 @@ int vector_isempty(vector* v)
 }
 
 
-int vector_get(vector* v, int index)
+T_Elem vector_get(vector* v, int index)
 {
     if(v==NULL)return -1;
     if(index >= v->maxsize)return -2;
@@ -68,7 +76,7 @@ int vector_get(vector* v, int index)
     return v->a[index];
 }
 
-int vector_set(vector* v, int index, int value) 
+T_Elem vector_set(vector* v, int index, int value) 
 {
     if(v==NULL)return -1;
     if(index >= v->maxsize)return -2;
@@ -84,6 +92,71 @@ int vector_add(vector* v, int value)
 {
     if(v==NULL)return -1;
 
+    int i=vector_size(v);
+    if(vector_isfull(v))
+    {
+        vector* temp = (vector*) realloc(v, sizeof(vector) + (v->maxsize + 1) * sizeof(int));
+        v=temp;
+        v->maxsize=v->maxsize+1;
+        vector_set(v,i,value);
+        return 1;
+    }
+
+    return 0;
+
+}
+
+void vector_resize(vector** v)
+{
+    if(*v==NULL) return;
+
+    int i=vector_size(*v);
+
+    if(vector_isfull(*v))
+    {
+        vector* temp = (vector*) realloc(*v, sizeof(vector) + ((*v)->maxsize + 1) * sizeof(int));
+        *v=temp;
+        (*v)->maxsize=(*v)->maxsize+1;
+    
+
+    }
+}
+
+void vector_remove(vector*v, int index)
+{
+    if(v==NULL)return ;
+
+    for(int i = index; i < v->size - 1; i++)
+    {
+        v->a[i] = v->a[i + 1];
+    }
+
+    v->size--;
+    
+}
+
+int vector_insert(vector* v, int value, int index)
+{
+    if(v==NULL)return -1;
+    int dim=vector_size(v);
+    if(vector_isfull(v))
+    {
+        vector_resize(&v);
+        for(int j=dim; j>index; j--)
+        {
+         v->a[j]=v->a[j-1];
+        }
+    }else{
+        for(int i = dim; i>index; i--)
+        {
+            v->a[i]=v->a[i-1];
+        }
+        vector_set(v,index,value);
+    }
+
+
+
+    return 1;
 
 }
 
